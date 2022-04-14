84a6b98238597a0f2acd3650bf4401b1e072b600
ShreddingThrasher <100221813+ShreddingThrasher@users.noreply.github.com>
2022-04-14 20:57:49 +0300

    Initial commit

---
+++ Nested Loops - Lab/SumOfTwoNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace NestedLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            bool magic = false;
+            int combinations = 0;
+
+            for (int i = start; i <= end; i++)
+            {
+                for (int j = start; j <= end; j++)
+                {
+                    combinations++;
+                    if (i + j == magicNumber)
+                    {
+                        magic = true;
+                        Console.WriteLine($"Combination N:{combinations} ({i} + {j} = {magicNumber})");
+                        i = end + 1;
+                        break;
+                    }
+                }
+            }
+
+            if (!magic)
+            {
+                Console.WriteLine($"{combinations} combinations - neither equals {magicNumber}");
+            }
+
+        }
+    }
+}
---
+++ Nested Loops - Lab/Travelling/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace NestedLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            string input;
+
+            while (destination != "End")
+            {
+                double destinationPrice = double.Parse(Console.ReadLine());
+                double money = 0;
+
+                while (money < destinationPrice)
+                {
+                    input = Console.ReadLine();
+
+                    if (input == "End")
+                    {
+                        break;
+                        destination = input;
+                    }
+                    money += double.Parse(input);
+
+                    if (money >= destinationPrice)
+                    {
+                        Console.WriteLine($"Going to {destination}!");
+                        break;
+                    }
+                }
+
+                destination = Console.ReadLine();
+            }
+
+        }
+    }
+}
